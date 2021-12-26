0c5c5ef53eae4c48babb5dcde891db69e66a7aa8
Abizer Nasir <abizern@abizern.org>
2021-12-26 14:52:48 +0000

    2020 minor file tidy-up

---
+++ 2020/day04.lisp
@@ -0,0 +1,7 @@
+(defpackage :aoc-20-4
+  (:use :cl)
+  (:documentation "--- Day 4: Passport Processing ---"))
+
+(in-package #:aoc-20-4)
+
+(defparameter *input* (uiop:read-file-lines "day04.txt"))
---
+++ 2020/day05.lisp
@@ -0,0 +1,5 @@
+(defpackage :aoc-20-5
+  (:use :cl)
+  (:documentation "--- Day 5: Binary Boarding ---"))
+
+(in-package #:aoc-20-5)
