020827055a26df18616409e86221d5fde3d8658f
Abizer Nasir <abizern@abizern.org>
2021-12-28 22:35:53 +0000

    Add file for 2021

--- 2021/day02.lisp
+++ 2021/day02.lisp
@@ -1,18 +1,30 @@
+(ql:quickload :cl-ppcre)
+
 (defpackage :aoc-21-2
- (:use :cl)
- (:documentation "--- Day 2: Dive! ---"))
+  (:use :cl)
+  (:import-from :cl-ppcre :register-groups-bind)
+  (:documentation "--- Day 2: Dive! ---"))
 
 (in-package :aoc-21-2)
 
 (defparameter *raw-input* (uiop:read-file-lines "day02.txt"))
 
-(defparameter *input* )
+(defun parse-line (line)
+  (register-groups-bind (instruction
+                         (#'parse-integer value))
+      ("^\(\\w+\) \(\\d+\)$" line)
+    (cons instruction value)))
+
+(defparameter *input* (mapcar #'parse-line *raw-input*))
 
 (defparameter +sample+ '())
+
+(defun loop-part-1 (instructions)
+  )
 
 
 (defun part1 (&optional (input *input*))
   )
 
 (defun part2 (&optional (input *input*))
-  )
+  )                                     
