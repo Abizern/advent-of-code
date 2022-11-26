989c4fa434b1a6ca084a63573cc1259191832949
Abizer Nasir <abizern@mac.com>
2022-11-26 15:33:34 +0000

    Setup for 2015 02

---
+++ 2015/02-I-Was-Told-There-Would-Be-No-Math/day02.lisp
@@ -0,0 +1,20 @@
+(defpackage :aoc-2015-02
+   (:use :cl)
+  (:export #:part1
+           #:part2)
+  (:documentation ""))
+
+(in-package :aoc-2015-02
+            )
+
+(defparameter *raw-input* (uiop:read-file-lines (merge-pathnames "input.txt")))
+
+(defparameter *input* ())
+
+(defparameter *example* '())
+
+(defun part1 (&optional (input *input*))
+  )
+
+(defun part2 (&optional (input *input*))
+  )
