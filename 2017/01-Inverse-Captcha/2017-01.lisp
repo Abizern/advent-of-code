2860eb3e3809fd0929f482a9b0bc671d4de53c72
Abizer Nasir <abizern@mac.com>
2022-11-27 23:48:33 +0000

    Create folder for 2017-01 solutions

---
+++ 2017/01-Inverse-Captcha/2017-01.lisp
@@ -0,0 +1,19 @@
+(defpackage :aoc-2017-01
+  (:use :cl)
+  (:export #:part1
+           #:part2)
+  (:documentation ""))
+
+(in-package :aoc-2017-01)
+
+(defparameter *raw-input-2017-01* (uiop:read-file-lines (merge-pathnames "input.txt")))
+
+(defparameter *input-2017-01* ())
+
+(defparameter *example-2017-01* '())
+
+(defun part1 (&optional (input *input-2017-01*))
+  )
+
+(defun part2 (&optional (input *input-2017-01*))
+  )
