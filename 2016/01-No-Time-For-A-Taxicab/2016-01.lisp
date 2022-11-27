60ac755ac5868e13e340806e78b21856c269c9ac
Abizer Nasir <abizern@mac.com>
2022-11-27 23:46:30 +0000

    Update 2016-01

--- 2016/01-No-Time-For-A-Taxicab/2016-01.lisp
+++ 2016/01-No-Time-For-A-Taxicab/2016-01.lisp
@@ -1,17 +1,19 @@
 (defpackage :aoc-2016-01
   (:use :cl)
+  (:export #:part1
+           #:part2)
   (:documentation ""))
 
 (in-package :aoc-2016-01)
 
-(defparameter *raw-input* (uiop:read-file-string "input.txt"))
+(defparameter *raw-input-2016-01* (uiop:read-file-lines (merge-pathnames "input.txt")))
 
-(defparameter *input* ())
+(defparameter *input-2016-01* ())
 
-(defparameter *example* '())
+(defparameter *example-2016-01* '())
 
-(defun part1 (&optional (input *input*))
+(defun part1 (&optional (input *input-2016-01*))
   )
 
-(defun part2 (&optional (input *input*))
+(defun part2 (&optional (input *input-2016-01*))
   )
