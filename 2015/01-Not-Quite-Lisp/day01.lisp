8683d506fd7091766e727786a5eb50db561e6582
Abizer Nasir <abizern@mac.com>
2022-11-27 22:25:17 +0000

    Minor tweakage

--- 2015/01-Not-Quite-Lisp/day01.lisp
+++ 2015/01-Not-Quite-Lisp/day01.lisp
@@ -1,9 +1,9 @@
 (defpackage :aoc-2015-01
   (:use :cl)
   (:export #:part1
-           #:part2
-           #:*raw-input*)
-  (:documentation ""))
+           #:part2)
+  (:documentation "--- Day 1: Not Quite Lisp ---
+Nothing fancy, just looping through the string"))
 
 (in-package :aoc-2015-01)
 
@@ -13,25 +13,21 @@
   (loop for c across input
         counting (char= c #\() into up
         counting (char= c #\)) into down
-        finally (return (- up down)))
-  )
+        finally (return (- up down))))
 
 (defun part2 (&optional (input *raw-input*))
   (loop for c across input
         for i from 1
         with floor =  0
-                  do (if (char= c #\()
-                       (incf floor 1)
-                       (decf floor 1))
-                   (when (= floor -1)
-                     (return i))
-                   finally (return floor)
-        )
-  )
+        do (if (char= c #\()
+               (incf floor 1)
+             (decf floor 1))
+        (when (= floor -1)
+          (return i))
+        finally (return floor)))
 
 (defun test-part1 ()
   (= (part1) 232))
 
 (defun test-part2 ()
   (= (part2) 1783))
-
